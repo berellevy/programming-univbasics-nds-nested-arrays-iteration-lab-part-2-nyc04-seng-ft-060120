ba2a0de8b660568279ec163ac0c00a14ab414b68
Berel Levy <berellevy@gmail.com>
2020-05-20 16:00:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,18 @@
 def find_min_in_nested_arrays(src)
+  smallies = []
+  row_index = 0
+  while row_index < src.length do
+    element_index = 0
+    least = 100
+    while element_index < src[row_index].length do
+      if least < src[row_index][element_index] 
+        least = src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    smallies << least
+    row_index += 1
+  end
   
-
+smallies
 end
