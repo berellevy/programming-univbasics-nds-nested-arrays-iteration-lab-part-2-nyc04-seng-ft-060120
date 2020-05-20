afead1a37c37d736aef17efa501088ecbcc55cec
Berel Levy <berellevy@gmail.com>
2020-05-20 16:01:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0
     least = 100
     while element_index < src[row_index].length do
-      if least < src[row_index][element_index]
+      if least > src[row_index][element_index]
         least = src[row_index][element_index]
       end
       element_index += 1
